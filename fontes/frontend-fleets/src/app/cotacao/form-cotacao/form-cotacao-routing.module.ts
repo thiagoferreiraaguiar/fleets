f9d8162fb63412a9cc958fd469efb1cb3a31eedb
Thiago <thiagoferreiradeaguiar@gmail.com>
2022-06-10 22:13:01 -0300

    criando tela de cadastro manual de cotação
    criando tela de cadastro manual de cotação

--- fontes/frontend-fleets/src/app/cotacao/form-cotacao/form-cotacao-routing.module.ts
+++ fontes/frontend-fleets/src/app/cotacao/form-cotacao/form-cotacao-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormCotacaoComponent } from './form-cotacao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormCotacaoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- fontes/frontend-fleets/src/app/cotacao/form-cotacao/form-cotacao.module.ts
+++ fontes/frontend-fleets/src/app/cotacao/form-cotacao/form-cotacao.module.ts
@@ -2,13 +2,39 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormCotacaoRoutingModule } from './form-cotacao-routing.module';
-
+import { FormCotacaoComponent } from './form-cotacao.component';
+import { CotacaoService } from '../cotacao.service';
+import { PanelModule } from 'primeng/panel';
+import { FileUploadModule } from 'primeng/fileupload';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TabViewModule } from 'primeng/tabview';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @NgModule({
-  declarations: [],
+  declarations: [FormCotacaoComponent],
   imports: [
     CommonModule,
-    FormCotacaoRoutingModule
-  ]
+    FormCotacaoRoutingModule,
+    PanelModule,
+    FileUploadModule,
+    RadioButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TabViewModule,
+    CardModule,
+    InputTextModule,
+    CalendarModule,
+    DropdownModule,
+    InputNumberModule,
+    ProgressBarModule
+  ],
+  exports: [FormCotacaoComponent],
+  providers: [CotacaoService]
 })
 export class FormCotacaoModule { }
